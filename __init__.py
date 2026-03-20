05f5f8470f8c74d1893aa9e03558e13916e3752c
RSD-programmist <117760231+RSD-programmist@users.noreply.github.com>
2026-03-20 16:09:21 +0500

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Core module - ядро приложения
-"""
-from .app import PortableGroupPolicyApp
-from .registry_utils import validate_registry_path, sanitize_reg_value, RegistryUtils
-
-__all__ = ['PortableGroupPolicyApp', 'validate_registry_path', 'sanitize_reg_value', 'RegistryUtils']
